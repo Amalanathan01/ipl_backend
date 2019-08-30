a0c6177167ac05aa068630cc4c22cb0cf8874854
vidhuvj <vidhusrivenu9@gmail.com>
2019-08-30 21:56:22 +0530

    663870-Added model files for match and delivery

---
+++ models/deliveryModel.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+ 
+var deliverySchema = new Schema({
+    id : {type : Number},
+    //match_id : {type : ForeignKey},
+    inning : {type : Number},
+    batting_team : {type : String},
+    bowling_team : {type : String},
+    date : {type : Date},
+    over : {type : Number},
+    ball : {type : Number},
+    batsman : {type : String},
+    non_striker : {type : String},
+    bowler : {type : String},
+    is_super_over : {type : Boolean},
+    wide_runs : {type : Number},
+    bye_runs : {type : Number},
+    legbye_runs : {type : Number},
+    noball_runs : {type : Number},
+    penalty_runs : {type : Number},
+    batsman_runs : {type : Number},
+    extra_runs : {type : Number},
+    total_runs : {type : Number},
+    player_dismissed : {type : String},
+    dismissal_kind : {type : String},
+    fielder : {type : String}
+});
+
+const Delivery = mongoose.model('delivery', deliverySchema)
+module.exports = Delivery;
